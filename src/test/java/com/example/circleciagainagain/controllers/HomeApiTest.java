603994e3807fa94c1531f756db9cadb8775a3baa
Devin Leyba <dleyba042@gmail.com>
2022-11-29 14:26:01 -0800

    test merger

---
+++ src/test/java/com/example/circleciagainagain/controllers/HomeApiTest.java
@@ -0,0 +1,33 @@
+package com.example.circleciagainagain.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HomeApiTest
+{
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate template;
+
+    @Test
+    public void homeTest()
+    {
+        //create a request for localhost
+        String url = "http://localhost:" + port + "/api/v1/home";
+
+        HttpEntity request = new HttpEntity(new HttpHeaders());
+        ResponseEntity<String> response = template.exchange(url, HttpMethod.GET,request,String.class);
+
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
+        assertEquals(response.getBody(),"Hello World!");
+    }
+}
