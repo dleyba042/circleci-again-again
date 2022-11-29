97a80ba1ba2ad5e60419489c793ac51f5862b5dd
Devin Leyba <dleyba042@gmail.com>
2022-11-29 14:15:16 -0800

    CIRCLE CI AGAIN AGAIN

---
+++ src/main/java/com/example/circleciagainagain/CircleCiAgainAgainApplication.java
@@ -0,0 +1,13 @@
+package com.example.circleciagainagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircleCiAgainAgainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CircleCiAgainAgainApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/circleciagainagain/CircleCiAgainAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.circleciagainagain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircleCiAgainAgainApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
